81999b969a6cf10f02d99bacb17cc37dc8c29f31
habeeb <habeebalao101@gmail.com>
2022-11-30 00:01:46 +0000

    update

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello again from Dockerised Flask"
+    return "Hello again from Dockerised Flask!"
 
 @app.route("/route32a")
 def route32a():
@@ -16,4 +16,4 @@ def route():
     return "Hello from the {}".format(number)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port='8080')
+    app.run(host='0.0.0.0',port='3000')
